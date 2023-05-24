515eca53bc0b8ae2a0769f587d35761357a98b2b
Allan Kyagulanyi <allankyagulanyi8@gmail.com>
2023-05-24 12:29:04 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+
+    </div>
+
+
+
+
+        </div>
+    </div>
+                <marquee onmouseover="this.stop()" onmouseout="this.start()">Hello you can message <a href="https://api.whatsapp.com/send/?phone=256781928954&amp;text&amp;type=phone_number&amp;app_absent=0">WhatsApp</a> to do any kind of project.</marquee>
+
+
+</body>
+</html>
+
+<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script type="text/javascript" src="./assets/scripts/main.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/myjs.js"></script>
+<script type="text/javascript" src="js/ajax.js"></script>
+<script type="text/javascript" src="js/sweetalert.js"></script>
+<!-- <script type="text/javascript" src="js/myjs.js"></script> -->
+
+
+
+
